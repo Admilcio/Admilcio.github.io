2986a18bcb7a7393d5ca58c22a1ba19763b3e333
Admilcio da Mata <113069526+Admilcio@users.noreply.github.com>
2023-07-11 00:14:14 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".control").each(function() {
+      $(this).on("click", function() {
+        $(".active-btn").removeClass("active-btn");
+        $(this).addClass("active-btn");
+        $(".active").removeClass("active");
+        $("#" + $(this).data("id")).addClass("active");
+      });
+    });
+  
+    $(".theme-btn").on("click", function() {
+      $("body").toggleClass("light-mode");
+    });
+  });
